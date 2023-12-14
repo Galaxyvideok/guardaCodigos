1825995d1de466c65db9bbab864f1e2222c7705e
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-12-14 07:14:51 -0300

    efdssd

--- testes/teste.c
+++ testes/teste.c
@@ -6,7 +6,7 @@
 #define INICIO "--------INICIO--------"
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
-#define MAX 6
+#define MAX 10
 
 void trocar(int *vetor, int element1, int element2){
     int aux;
@@ -262,55 +262,6 @@ void quickSort(int *vetor, int ini, int fim){
     }
 }
 
-// int pesquisarMaior(int *vetor, int quanti){
-//     int i;
-//     int maior = 0;
-//     for (i = 0; i < quanti; i++){
-//         if (vetor[i] > vetor[maior]){
-//             maior = i;
-//         }
-        
-//     }
-//     return maior;
-// }
-
-// void selectSort(int *vetor, int quanti){
-//     int indiceMaior;
-//     int f;
-//     for (f = quanti; f > 1; f--){
-//         indiceMaior = pesquisarMaior(vetor,f);
-//         if (indiceMaior != f - 1){
-//             trocar(vetor,indiceMaior,f-1);
-//         }
-        
-//     }
-// }
-
-// void atualizarHeap(int *vetor, int raiz, int quanti){
-//     int filhoEsquerda = 2*raiz + 1;
-//     int filhoDireita = 2*raiz + 2;
-//     int max;
-//     if (){
-//         /* code */
-//     }
-    
-// }
-
-// void construir(){
-
-// }
-
-// void heapSort(int *vetor, int quanti){
-
-//     construir()
-//     while (quanti){
-//         /* code */
-//     }
-    
-    
-    
-// }
-
 int maiorValor(int *vetor, int quanti){
     int maior = 0;
     int i;
@@ -359,50 +310,47 @@ void atualizarHeap(int *vetor, int quanti, int indiceRaiz){
     }
 }
 
-void contruirHeap(int *vetor, int n){
+void contruirHeap(int *vetor, int quanti){
     int i;
-    for (i = n/2 - 1; i >= 0; i--){
-        atualizarHeap(vetor,n,i);
+    for (i = quanti/2 - 1; i >= 0; i--){
+        atualizarHeap(vetor,quanti,i);
     }
-    
 }
 
 void heapSort(int *vetor, int quanti){
     int i;
-        contruirHeap(vetor,quanti);
+    contruirHeap(vetor,quanti);
     for (i = quanti-1; i > 0; i--){
         trocar2(&vetor[i],&vetor[0]);
         atualizarHeap(vetor,i,0);
     }
-    
 }
 
 void selectSort(int *vetor, int quanti){
     int indiceMaior;
-    if (quanti > 1){
-        indiceMaior = maiorValor(vetor,quanti);
-        trocar(vetor,indiceMaior,quanti-1);
-        selectSort(vetor,quanti-1);
-    }
-}
-
-void selectSortNormal(int *vetor, int quanti){
-    int indiceMaior;
-    int f;
-    int trocas = 0;
-    for (f = quanti; f > 1; f--){
-        indiceMaior = maiorValor(vetor,f);
-        if (indiceMaior != f - 1){
-            trocar(vetor,indiceMaior,f-1);
+    int i, trocas = 0;
+    for (i = quanti; i > 1; i--){
+        indiceMaior = maiorValor(vetor,i);
+        if (indiceMaior != i - 1){
+            trocar(vetor,indiceMaior,i-1);
             trocas++;
         }
     }
-    printf("\nNumero de trocas: %d\n", trocas);
+    printf("\nNumero de trocas: %d\n",trocas);
+}
+
+void selectSortREC(int *vetor, int quanti){
+    int indiceMaior;
+    if (quanti > 1){
+        indiceMaior = maiorValor(vetor,quanti);
+        trocar(vetor,indiceMaior,quanti-1);
+        selectSortREC(vetor,quanti-1);
+    }
 }
 
 void intercalar(int *vetor, int ini1, int fim1, int ini2, int fim2){
     int tam = fim2 - ini1 + 1;
-    int vetorAux = (int) malloc (tam * sizeof(int));
+    int *vetorAux = (int*) malloc (tam * sizeof(int));
     int i1 = ini1;
     int i2 = ini2;
     int k = 0;
@@ -434,15 +382,10 @@ void intercalar(int *vetor, int ini1, int fim1, int ini2, int fim2){
 
 void mergeSort(int *vetor, int ini, int fim){
     int meio;
-    //CONDIÇAO DE PARADA
     if (ini < fim){
-        //DIVIDE AO MEIO
-        meio = (ini + fim)/2;
-        //ORDENAR O PRIMEIRO LADO
+        meio = (ini+fim)/2;
         mergeSort(vetor,ini,meio);
-        //ORDENAR O SEGUNDO LADO
         mergeSort(vetor,meio+1,fim);
-        //INTERCALAR
         intercalar(vetor,ini,meio,meio+1,fim);
     }
 }
@@ -455,8 +398,8 @@ void imprimirVetor(int *vet, int quanti){
 }
 
 int main() {
-    int vetor[MAX] = {1,2,3,8,9,1};
-    int quanti = 6;
+    int vetor[MAX] = {34,1,22,3,45,91,17,34,21,67};
+    int quanti = 10;
     // int indiceRes = 0;
     // int pesq;
     printf("\n%s\n", INICIO);
@@ -481,7 +424,8 @@ int main() {
     // combSortREC(vetor,quanti,quanti);
     // quickSort(vetor, 0, quanti-1);
     // selectSort(vetor,quanti);
-
+    // selectSortREC(vetor,quanti);
+    // heapSort(vetor,quanti);
     // mergeSort(vetor,0,quanti-1);
     printf("\nVetor Depois: \n");
     imprimirVetor(vetor,quanti);
